bd8a9fd899eaaddc0665d8141fe88ffd6cae4b3b
Oliver Kniffel <oliver.kniffel@mail.de>
2023-02-26 17:59:56 +0100

    Adding format method to NumberUtils

--- src/main/java/rpg/common/util/NumberUtils.java
+++ src/main/java/rpg/common/util/NumberUtils.java
@@ -5,9 +5,14 @@ package rpg.common.util;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.List;
+import java.util.Locale;
 
 public abstract class NumberUtils {
+
+  public static final String DECIMAL_FORMAT_PATTERN_DEFAULT = "#.#";
 
   public static double round(double value, int digits) {
     BigDecimal result = BigDecimal.valueOf(value).setScale(digits, RoundingMode.HALF_EVEN);
@@ -45,5 +50,15 @@ public abstract class NumberUtils {
   public static <T> T getEntry(List<T> list, int index) {
     int adjustedIndex = squeeze(index, 0, list.size() - 1);
     return list.get(adjustedIndex);
+  }
+
+  public static String format(final double value) {
+    return format(DECIMAL_FORMAT_PATTERN_DEFAULT, value);
+  }
+
+  public static String format(final String pattern, final double value) {
+    DecimalFormat format = new DecimalFormat(pattern, new DecimalFormatSymbols(Locale.ROOT));
+    format.setGroupingUsed(false);
+    return format.format(value);
   }
 }
--- src/test/java/rpg/common/util/NumberUtilsTest.java
+++ src/test/java/rpg/common/util/NumberUtilsTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class NumberUtilsTest {
 
   @Test
-  void test() {
+  void testRound() {
     Assertions.assertEquals(100.0, NumberUtils.round(100.1, 0));
     Assertions.assertEquals(100.0, NumberUtils.round(100.5, 0));
     Assertions.assertEquals(101.0, NumberUtils.round(100.6, 0));
@@ -17,5 +17,16 @@ class NumberUtilsTest {
     Assertions.assertEquals(100.12, NumberUtils.round(100.1234, 2));
     Assertions.assertEquals(100.123, NumberUtils.round(100.1234, 3));
     Assertions.assertEquals(100.124, NumberUtils.round(100.1235, 3));
+  }
+  
+  @Test
+  void testFormat() {
+    Assertions.assertEquals("1", NumberUtils.format(1.0));
+    Assertions.assertEquals("1.2", NumberUtils.format(1.22));
+    Assertions.assertEquals("1.2", NumberUtils.format(1.225));
+    Assertions.assertEquals("1000", NumberUtils.format(1000));
+    Assertions.assertEquals("10000", NumberUtils.format(10000));
+    Assertions.assertEquals("10000.1", NumberUtils.format(10000.123456789));
+    Assertions.assertEquals("10001", NumberUtils.format(10000.987654321));
   }
 }
